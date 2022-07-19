504a4902c2cdd509f7170369897cbf5919e70e56
Pushkal-vashishtha <pushkalvas730@gmail.com>
2022-07-20 01:04:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import './Coin.css';
+import Coin from './Coin';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [coins,setCoins]=useState([])
+  const [search,setSearch]=useState('')
+useEffect(()=>{
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  ).then(res =>{
+    setCoins(res.data)
+   
+  }).catch(error => alert("kuch toh gadbad hai"))
+
+},[]);
+
+const handleChange=e =>{
+  setSearch(e.target.value)
+}
+const filteredCoins =coins.filter(coin =>
+  coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+ return (
+    <div className="coin-app">
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input type="text" placeholder="search" className='coin-input' onChange={handleChange}>
+
+          </input>
+        </form>
+      </div>
+      {filteredCoins.map(coin =>{
+        return(
+          <Coin
+           key={coin.id}
+           name={coin.name}
+           image={coin.image}
+           symbol={coin.symbol}
+           marketcap={coin.market_cap}
+           priceChange={coin.price_change_percentage_24h}
+           volume={coin.total_volume}
+           />
+        )
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
